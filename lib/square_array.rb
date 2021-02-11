81e94e1c61a7b0361295c01ccbbbf3c326640093
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-11 17:20:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,8 @@ def square_array(array)
   new_array = []
   
   while counter < array.length do
-    new_array.push(array[count]**2)
-    count += 1 
+    new_array.push(array[counter]**2)
+    counter += 1 
   end
   
   return new_array
